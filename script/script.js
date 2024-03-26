c4ab244f3983a5e621686bdb0da3271b81104ccb
wmancaniello <wmancaniello@gmail.com>
2024-03-26 11:11:51 +0100

    add other discount application under comment

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@
 // Chiedo numero di km, che sarà una string
 let numKm = prompt(
   "Benvenuto in TrainBOOL! Quanti Km dovrai percorrere?"
-); //string
+); //string | null
 // Converto il dato fornito in number
 numKm = parseFloat(numKm); //number
 console.log(numKm, typeof numKm);
@@ -14,7 +14,7 @@ console.log(numKm, typeof numKm);
 // Chiedo l'età del passeggero, che sarà una string
 let age = prompt(
   "Perfetto, potresti indcarmi qual'è la tua età? Così da applicare, se possibile, eventuali sconti!"
-); //string
+); //string | null
 // Converto il dato fornito in number
 age = parseInt(age); //number
 console.log(age, typeof age);
@@ -38,6 +38,41 @@ if (age < 18) {
     totalPrice = totalPrice * 0.6;
 }
 console.log(totalPrice, typeof totalPrice);
+
+// Altra modalità di applicazione sconto
+
+// let discount;
+// if (age < 18) {
+//   discount = 20;
+// } else if (age > 65) {
+//   discount = 40;
+// } else {
+//   discount = 0;
+// }
+
+// console.log('discount in %', discount );
+// const totalDiscount = totalPrice * discount / 100;
+// console.log('discount in euro', totalDiscount);
+
+// const finalPrice = totalPrice - totalDiscount
+// console.log('prezzo finale', finalPrice);
+
+// OUTPUT
+// const resultMessage = `
+//   <dl>
+//     <dt>Prezzo base:</dt>
+//     <dd>€ ${totalPrice.toFixed(2) /* string */}</dd>
+
+//     <dt>Sconto:</dt>
+//     <dd>${discounr /* number */} % (€ ${totalDiscount.toFixed(2)})</dd>
+
+//     <dt>Prezzo finale</dt>
+//     <dd>€ ${fianl.price.toFixed(2) /* string */}</dd>
+//   </dl>
+// `;
+// Stampa
+// ???
+// Altra modalità di applicazione sconto
 
 
 // STAMPA
